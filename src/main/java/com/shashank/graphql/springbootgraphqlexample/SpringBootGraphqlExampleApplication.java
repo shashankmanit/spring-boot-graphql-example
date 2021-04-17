315f96fdd986b97824fdcd7192c244ed694bddcc
Shashank <shashankmani02@gmail.com>
2021-04-17 11:39:04 +0530

    Add pom.xml

---
+++ src/main/java/com/shashank/graphql/springbootgraphqlexample/SpringBootGraphqlExampleApplication.java
@@ -0,0 +1,13 @@
+package com.shashank.graphql.springbootgraphqlexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGraphqlExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGraphqlExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shashank/graphql/springbootgraphqlexample/SpringBootGraphqlExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shashank.graphql.springbootgraphqlexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGraphqlExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
